d02122f03cd4b4a93cd4ef36d7f22605b7ac91cf
Louisnt52 <luisfelipenunezhumana@gmail.com>
2025-11-09 22:51:59 -0400

    add routes for Product, Forms, and Users

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./pages/home/home')
+    },
+    {
+        path: 'product',
+        loadComponent: () => import('./pages/product/product')
+    },
+    {
+        path: 'forms',
+        loadComponent: () => import('./pages/forms/forms')
+    },
+    {
+        path: 'users',
+        loadComponent: () => import('./pages/users/users')
+    },
+    {
+        path: '**',
+        redirectTo: 'product'
+    }
+];
